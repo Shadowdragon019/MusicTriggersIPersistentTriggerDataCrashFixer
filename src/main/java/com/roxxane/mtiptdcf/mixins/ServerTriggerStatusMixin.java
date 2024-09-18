1813600588fd8d8f3bb7f3674dff45c5a590e0c3
Sammy [any/all] <Shadowdragon019019@gmail.com>
2024-09-18 13:57:36 -0500

    Fix things related to SERVER_DATA from causing CME crashes

--- src/main/java/com/roxxane/mtiptdcf/mixins/ServerTriggerStatusMixin.java
+++ src/main/java/com/roxxane/mtiptdcf/mixins/ServerTriggerStatusMixin.java
@@ -7,8 +7,11 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
+@SuppressWarnings({"SynchronizeOnNonFinalField", "unchecked", "SuspiciousMethodCalls"})
 @Mixin(ServerTriggerStatus.class)
 abstract class ServerTriggerStatusMixin {
     @Shadow(remap = false) @Final private static Map<String, ServerTriggerStatus> SERVER_DATA;
@@ -19,17 +22,52 @@ abstract class ServerTriggerStatusMixin {
         method = "runServerChecks", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z"),
         remap = false)
     private synchronized static boolean runServerChecksRedirect(Iterator<?> instance) {
-        var toRemove = new ArrayList<String>();
+        synchronized (SERVER_DATA) {
+            var toRemove = new ArrayList<String>();
 
-        for (var entry : SERVER_DATA.entrySet())
-            if (entry.getValue().isValid())
-                ((ServerTriggerStatusMixin) (Object) entry.getValue()).runChecks();
-            else
-                toRemove.add(entry.getKey());
+            for (var entry : SERVER_DATA.entrySet())
+                if (entry.getValue().isValid())
+                    ((ServerTriggerStatusMixin) (Object) entry.getValue()).runChecks();
+                else
+                    toRemove.add(entry.getKey());
 
-        for (var key : toRemove)
-            SERVER_DATA.remove(key);
+            for (var key : toRemove)
+                SERVER_DATA.remove(key);
 
-        return false;
+            return false;
+        }
+    }
+
+    @Redirect(method = "initializePlayerChannels", at = @At(value = "INVOKE",
+        target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
+        remap = false)
+    private static <K, V> V initializePlayerChannelsRedirect(Map<K, V> instance, K k, V v) {
+        synchronized (SERVER_DATA) {
+            return (V) SERVER_DATA.put((String) k, (ServerTriggerStatus) v);
+        }
+    }
+
+    @Redirect(method = "decodeDynamicInfo", at = @At(value = "INVOKE",
+        target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"), remap = false)
+    private static <K, V> V decodeDynamicInfoRedirect(Map<K, V> instance, Object o) {
+        synchronized (SERVER_DATA) {
+            return (V) SERVER_DATA.get(o);
+        }
+    }
+
+    @Redirect(method = "recordAudioData(Ljava/util/UUID;Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/item/ItemStack;",
+        at = @At(value = "INVOKE", target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"), remap = false)
+    private static <K, V> V recordAudioDataRedirect(Map<K, V> instance, Object o) {
+        synchronized (SERVER_DATA) {
+            return (V) SERVER_DATA.get(o);
+        }
+    }
+
+    @Redirect(method = "setPVP(Lnet/minecraft/server/level/ServerPlayer;Ljava/lang/String;)V",
+        at = @At(value = "INVOKE", target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"), remap = false)
+    private static <K, V> V setPVPRedirect(Map<K, V> instance, Object o) {
+        synchronized (SERVER_DATA) {
+            return (V) SERVER_DATA.get(o);
+        }
     }
 }
